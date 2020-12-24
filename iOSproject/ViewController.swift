b6592acc2a197bbab9abb607b68143008f9afdd4
Sarvar <sarvar8899@icloud.com>
2020-12-24 09:37:56 +0500

    Initial Commit

---
+++ iOSproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSproject
+//
+//  Created by mac on 24.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
